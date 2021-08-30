55818e74658e2da6e2ae73a64d310f13d84076e5
Alexandria (Alex) Barghi <bgamer50@gmail.com>
2021-08-30 03:57:49 +0000

    update has step to allow custom acquirer

--- step/property/HasStep.h
+++ step/property/HasStep.h
@@ -20,6 +20,12 @@ class HasStep : public TraversalStep {
 		// Predicate
 		P predicate = P(P::Comparison::EQ, boost::any());
 
+		// Acquirer
+		std::function<boost::any(GraphTraversalSource*, Vertex*, std::string&)> acquirer = [](GraphTraversalSource* src, Vertex* v, std::string& key){
+			VertexProperty* vp = static_cast<VertexProperty*>(v->property(key));
+			return vp == nullptr ? boost::any() : vp->value();
+		};
+
 	public:
 		HasStep(std::string property_key_or_label, P predicate)
 		: TraversalStep(FILTER, HAS_STEP) {
@@ -39,38 +45,42 @@ class HasStep : public TraversalStep {
 			return this->predicate.operand;
 		}
 
+		void set_acquirer(std::function<boost::any(GraphTraversalSource* src, Vertex*, std::string&)> acquirer) {
+			this->acquirer = acquirer;
+		}
+
 		/**
             This is used for the expansion of has(prop) and has(prop, val)
             It tests whether the Vertex in question has the property/value
             in question and returns true if this is in fact the case.
 		**/
 		bool test(Vertex* v, GraphTraversalSource* src) {
-			VertexProperty* vp = static_cast<VertexProperty*>(v->property(property_key_or_label));
+			boost::any property_val = this->acquirer(src, v, this->property_key_or_label);
 			boost::any val = this->predicate.operand;
 			switch(this->predicate.comparison) { // betting on a good branch predictor here
 				case P::Comparison::EQ:
-					if(val.empty()) return vp == nullptr;
-					return src->test_equals(vp->value(), val);
+					if(val.empty()) return property_val.empty();
+					return src->test_equals(property_val, val);
 					break;
 				case P::Comparison::NEQ:
-					if(val.empty()) return vp != nullptr;
-					return !src->test_equals(vp->value(), val);
+					if(val.empty()) return !property_val.empty();
+					return !src->test_equals(property_val, val);
 					break;
 				case P::Comparison::GTE:
-					return src->test_compare(vp->value(), val) >= 0;
+					return src->test_compare(property_val, val) >= 0;
 					break;
 				case P::Comparison::GT:
-					return src->test_compare(vp->value(), val) > 0;
+					return src->test_compare(property_val, val) > 0;
 					break;
 				case P::Comparison::LTE:
-					return src->test_compare(vp->value(), val) <= 0;
+					return src->test_compare(property_val, val) <= 0;
 					break;
 				case P::Comparison::LT:
-					return src->test_compare(vp->value(), val) < 0;
+					return src->test_compare(property_val, val) < 0;
 					break;
 				case P::Comparison::BETWEEN: {
 					std::pair<boost::any, boost::any> p = boost::any_cast<std::pair<boost::any, boost::any>>(val);
-					return src->test_compare(vp->value(), p.first) >= 0 && src->test_compare(vp->value(), p.second) < 0;
+					return src->test_compare(property_val, p.first) >= 0 && src->test_compare(property_val, p.second) < 0;
 					break;
 				}
 				default:
